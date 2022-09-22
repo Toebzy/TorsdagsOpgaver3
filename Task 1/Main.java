1b18d3ec0b5ae8e74d1a1efd89ae5eaa81024fe6
Toebzy <Tobiascarlsen@live.dk>
2022-09-22 15:39:59 +0200

    Task 1

---
+++ Task 1/Main.java
@@ -0,0 +1,11 @@
+class Main{
+
+	public static void main (String [] args) {
+		
+	palindrome pal = new palindrome("Den laks skal ned");
+	
+
+	}
+
+
+}
---
+++ Task 1/palindrome.java
@@ -0,0 +1,20 @@
+class palindrome {
+
+    String pal = "makam";
+	String pal1= pal.toLowerCase();
+
+	palindrome(String pal) {
+	this.pal = pal;
+	}
+
+	String ifPalindrome{
+	if (pal1.charAt(0)==pal1.charAt(pal1.length()-1) && (pal1.charAt(1)==pal1.charAt(pal1.length()-2)) && (pal1.charAt(2)==pal1.charAt(pal1.length()-3){
+	return pal;
+	 	
+	}
+	 
+	return false;
+	
+
+
+}
